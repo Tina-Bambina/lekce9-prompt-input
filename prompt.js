c7913e452628e8d0f89f6402b2ee85aec835711c
Tina-Bambina <speleerska@gmail.com>
2022-04-12 21:06:43 +0200

    testing console log remove

--- prompt.js
+++ prompt.js
@@ -1,15 +1,13 @@
-console.log('fd');
-
 let sirka = prompt('Zadej sirku pracky v cm', 1);
 let vyska = prompt('Zadej vysku pracky v cm', 1);
 let hloubka = prompt('Zadej hloubku pracky v cm', 1);
 
 
-let misnostSirka = 62;
+let mistnostSirka = 62;
 let mistnostVyska = 70;
 let mistnostHloubka = 80;
 
-if (sirka<=misnostSirka && vyska<=mistnostVyska && hloubka<=mistnostHloubka)
+if (sirka<=mistnostSirka && vyska<=mistnostVyska && hloubka<=mistnostHloubka)
     alert(`Pracka o sirce ${sirka}, vysce ${vyska} a hloubce ${hloubka} se nam domu vejde`);
 
 else 
